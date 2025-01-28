88b65c50d08090322e2ee13949b7121c3d666603
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-01-28 14:03:48 -0500

    bee movie

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main>
+    <h1>Big Bzzzzness, by Barry B. Benson</h1>
+    <!--Bee Movie shop!!!-->
+    <!--What do I want to be my components?-->
+    <!--Cards of Bee Movie characters should be a component (products to sell, or a dating app?) along with a heading introducing the site and cards for the shopping cart. (Shopping cart should also be a component?)-->
+  </main>
 </template>
 
 <style></style>
